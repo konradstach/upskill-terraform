4cbd11260197ac3fc8aa72fdf9df6b9d2e03e419
konradstach <konrad.stach00@gmail.com>
2022-04-27 11:31:27 +0200

    Move s3 related resorces to another file

--- main.tf
+++ main.tf
@@ -1,49 +1,5 @@
-resource "aws_s3_bucket" "tf-upskill-bucket" {
-  bucket = var.s3_bucket_name
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.tf-upskill-bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.tf-upskill-bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.tf-upskill-bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.tf-save-file-info.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-
-  depends_on = [aws_lambda_permission.allow_bucket]
-}
-
 resource "aws_sns_topic" "tf-s3-event-notification-topic" {
   name = var.notification-topic
-
-  policy = <<POLICY
-{
-    "Version":"2012-10-17",
-    "Statement":[{
-        "Effect": "Allow",
-        "Principal": { "Service": "s3.amazonaws.com" },
-        "Action": "SNS:Publish",
-        "Resource": "arn:aws:sns:*:*:tf-s3-event-notification-topic",
-        "Condition":{
-            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.tf-upskill-bucket.arn}"}
-        }
-    }]
-}
-POLICY
 }
 
 resource "aws_sns_topic_subscription" "email-target" {
---
+++ s3.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "tf-upskill-bucket" {
+  bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.tf-upskill-bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.tf-upskill-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.tf-upskill-bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.tf-save-file-info.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
